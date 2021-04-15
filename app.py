add8fa80a974e218526a03994e4c70e30526447f
Shen <doraemon9296@gmail.com>
2021-04-15 17:33:10 +0800

    f

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, url_for, jsonify, request
 app = Flask(__name__)
 # app.config['JSON_AS_ASCII'] = False
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    return render_template('test.html')
+
+@app.route('/test2', methods=['GET', 'POST'])
+def test2():
+    return render_template('test2.html')
 
 if __name__ == '__main__':
     app.run()
